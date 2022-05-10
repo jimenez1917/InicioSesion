f852ea3d0e791d45eeea7a0b439d296ab6f9f6d9
jimenez1917 <jimenez1917@gmail.com>
2022-05-10 14:37:49 -0500

    InicioSesion done

--- src/app.js
+++ src/app.js
@@ -1,212 +1,133 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const session = require("express-session");
-const MongoStore = require('connect-mongo');
-const path = require('path');
-const passport = require('passport');
-const LocalStrategy=require('passport-local').Strategy;
-const User = require('./models/User.js');
-const cookieParser = require('cookie-parser')
-const productosRouter= require('./routes/routes')
-const {Server}= require('socket.io');
-const bcrypt=require('bcrypt')
+import express from 'express'
+import session from 'express-session'
+import bcrypt from 'bcrypt'
+import mongoose from 'mongoose'
+import passport from 'passport'
+import {Strategy as LocalStrategy} from 'passport-local'
+import {dirname} from 'path'
+import {fileURLToPath} from 'url'
+import {User} from './models/user.js'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const app = express();
-const PORT = process.env.PORT||8080;
-const server = app.listen(PORT, () => console.log((`Listening on PORT ${PORT}`)));
+const app = express()
+const PORT = process.env.PORT||8080
+const server = app.listen(PORT,()=>console.log(`Listening on port ${PORT}`))
 
-const io = new Server(server);
+app.set('views', __dirname+'/views')
+app.set('view engine', 'ejs')
 
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-app.use('/api',productosRouter);
-app.use(cookieParser());
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-mongoose.connect(`mongodb+srv://jimenez1917:dajiru17@cluster0.vfuad.mongodb.net/passport?retryWrites=true&w=majority`,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-},err=>{
-    if(err) throw new Error(err);
-    console.log('Mongo Connected')
-})
 app.use(session({
-    store:MongoStore.create({
-        mongoUrl: `mongodb+srv://jimenez1917:dajiru17@cluster0.vfuad.mongodb.net/passport?retryWrites=true&w=majority`,
-        ttl:10000
-    }),
-    secret:'mongosecretcoderfeliz2022',
-    resave:false,
-    saveUninitialized:false,
-    // cookie:{
-    //     secure:false, //http o https
-    //     maxAge: 30
-    // }
+    secret:'cualquiercosa',
+    resave:true,
+    saveUninitialized:true,
+    cookie:{
+        expires: 30000
+    }
 }))
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
-passport.serializeUser((user,done)=>{
-    return done(null, user.id)
-})
-
-passport.deserializeUser((id, done)=>{
+passport.serializeUser((user,done)=>{return done(null,user)})
+passport.deserializeUser((id,done)=>{
     User.findById(id,(err,user)=>{
-        return done(err,user)
+        return done(err,user.id)
     })
 })
 
-const createHash = (password)=>{
+const createHash = (password) => {
     return bcrypt.hashSync(
         password,
         bcrypt.genSaltSync(10)
     )
 }
-passport.use('registro', new LocalStrategy(
-    {
-        passReqToCallback: true
-    },
-    (req,username,password,done)=>{
+
+const isUserLogged = (req,res,next)=>{
+    if(req.isAuthenticated()) return next()
+    res.redirect('/login')
+}
+
+passport.use('signupStrategy', new LocalStrategy(
+    (username, password, done)=>{
         User.findOne({username:username},(err,user)=>{
             if(err) return done(err)
-            if(user) return done(null, false, {message:"user already exists"});
+            if(user) return done(null,false,{message:'user already register'})
+
             const newUser = {
-                name: req.body.name,
                 username: username,
                 password: createHash(password)
             }
-            User.create(newUser, (err,userCreated)=>{
-                if(err) return done(err);
+
+            User.create(newUser,(err,userCreated)=>{
+                if(err) return done(err)
                 return done(null,userCreated)
             })
         })
     }
 ))
 
-passport.use('login', new LocalStrategy(
-    {
-        passReqToCallback: true
-    },
-    (req, username, password, done)=>{
-        console.log('strategy')
+passport.use('loginStrategy', new LocalStrategy(
+    (username, password, done)=>{
         User.findOne({username:username},(err,userFound)=>{
-            if(err) return done(err);
-            //validamos si el usuario no existe
-            if(!userFound) return done(null, false, {message:"user does not exists"})
-            //si encuentra el usuario, verificamos la contrasena
-            if(!bcrypt.compare(password, userFound.password)){
-                return done(null, false,{message:"invalid password"})
-            }
-            //abrir la sesion con el userFound
-            req.session.user = {username: userFound.username}
-            done(null, userFound);
+            if(err) return done(err)
+            if(!userFound) return done(null,false,{message:'user already exist'})
+            if(!bcrypt.compareSync(password,userFound.password)) return done(null,false,{message:'invalid password'})
+            return done(null,userFound)
         })
     }
 ))
 
-app.get('/', (req,res) => {
-    let cookie = req.cookies;
-    if(cookie.nombre)  {
-        res.redirect('/content');
-    }
-    res.sendFile('login.html', {
-        root: './views'
-      });
+const URL = 'mongodb+srv://jimenez1917:dajiru17@cluster0.vfuad.mongodb.net/passport?retryWrites=true&w=majority'
+
+mongoose.connect(URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+},(err)=>{
+    if(err) throw new Error('unable to connect')
+    console.log('connected to DB')
+})
+
+app.get('/',(req,res)=>{
+    res.render('home',{prueba:0})
 })
 app.get('/signup',(req,res)=>{
-    res.sendFile('singup.html',{
-        root: './views'
-    })
+    if(req.isAuthenticated()) return res.redirect('/profile')
+    res.render('signup')
 })
-app.post('/signupForm',passport.authenticate('registro',
-    {
-        failureRedirect:'/signup'
-    }),
-    (req,res)=>{
-        res.redirect('/perfil');
-})
-app.post("/loginForm",passport.authenticate('login',{
-    failureRedirect:'/login'
-}),(req,res)=>{
-    res.redirect('/content')
-})
-
-
-
 app.get('/login',(req,res)=>{
-    if(req.isAuthenticated()) return res.redirect('/content')
-    res.sendFile('login.html',{
-        root:'./views'
-    })
+    if(req.isAuthenticated()) return res.redirect('/profile')
+    res.render('login')
 })
-const isActiveSession = (req,res,next)=>{
-    if(req.session.user){
-        next();
-    } else{
-        res.send(
-            `<p>No autorizado</p></br><a href="/login">Iniciar sesion</a>`
-        )
+app.get('/profile',isUserLogged,(req,res)=>{
+    res.render('profile',{user:req.session.passport.user.username})
+})
+app.get('/logout',(req,res)=>{
+    if(req.isAuthenticated()) {
+        req.logOut()
+        res.render('logout') 
     }
-}
-app.get('/content',isActiveSession, (req,res)=>{
-    console.log(res);
-    res.sendFile('/index.html')
-})
-app.get('/prueba',(req,res)=>{
-    console.log(new LocalStrategy(
-        {
-            passReqToCallback: true
-        },
-        (req, username, password, done)=>{
-            User.findOne({username:username},(err,userFound)=>{
-                if(err) return done(err);
-                //validamos si el usuario no existe
-                if(!userFound) return done(null, false, {message:"user does not exists"})
-                //si encuentra el usuario, verificamos la contrasena
-                if(!bcrypt.compare(password, userFound.password)){
-                    return done(null, false,{message:"invalid password"})
-                }
-                //abrir la sesion con el userFound
-                req.session.user = {username: userFound.username}
-                done(null, userFound);
-            })
-        }
-    ));
+    res.redirect('/')
 })
 
-
-
-////////////////////////////////
-
-
-
-app.get('/content', (req,res) => {
-    let cookie;
-    if (req.cookies) {cookie = req.cookies}
-    if(!cookie.nombre)  {
-        res.redirect('/login');
-    }
-    res.sendFile('index.html', {
-        root: './views'
-      });
-});
-
-app.post('/logout',(req,res) => {
-    res.clearCookie('nombre');
-    res.sendFile('logout.html', {
-        root: './views'
-      });
+app.get('/userExist',(req,res)=>{
+    res.render('userExist')
+})
+app.get('/invalidPassword',(req,res)=>{
+    res.render('invalidPassword')
 })
 
+app.post('/signup',passport.authenticate('signupStrategy',{
+    failureRedirect:'/userExist'
+}),(req,res)=>{
+    res.redirect('/profile')
+})
 
-app.use(express.static('public'));
-
-io.on('connection', (socket) => {
-    socket.on('newMessage', (data) => {
-        io.emit('refreshChat',data);
-    })
-    socket.on('newProduct', (data) =>{
-        io.emit('refreshProducts',data);
-    })
+app.post('/login',passport.authenticate('loginStrategy',{
+    failureRedirect:'/invalidPassword'
+}),(req,res)=>{
+    res.redirect('/profile')
 })
--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,10 @@
-const mongoose = require('mongoose');
-const userCollection = "users";
+import mongoose from 'mongoose'
 
-const userSchema = new mongoose.Schema(
-    {   
-        name: {type:String},
-        username: {type: String, required:true},
-        password: {type: String, required: true}
-    }
-)
+const usersCollection = 'users'
 
-const User = mongoose.model(userCollection, userSchema);
+const userSchema = new mongoose.Schema({
+    username: {type:String, required:true},
+    password: {type:String, required:true},
+})
 
-exports.model=User;
+export const User = mongoose.model(usersCollection,userSchema)
